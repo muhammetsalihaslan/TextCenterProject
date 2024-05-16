e4fc678c919b9cbf2de9222f4ca602f730ccdc6d
Your Name <you@example.com>
2024-05-16 11:59:28 +0300

    to be continue

--- textproject/lib/widgets/custom_dropdown.dart
+++ textproject/lib/widgets/custom_dropdown.dart
@@ -8,44 +8,10 @@ class CustomDropDown extends StatefulWidget {
 }
 
 class _CustomDropDownState extends State<CustomDropDown> {
-  final TextEditingController firstPartExampleController =
-      TextEditingController();
-
-  String? _selectedItem = "tlc Exam";
-  final List<String> _dropdownItems = ['Option 1', 'Option 2', 'Option 3'];
+  
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            DropdownMenu<String>(
-              controller: firstPartExampleController,
-              enableFilter: true,
-              requestFocusOnTap: true,
-              label: _selectedItem,
-              inputDecorationTheme: const InputDecorationTheme(
-                filled: true,
-                contentPadding: EdgeInsets.symmetric(vertical: 5.0),
-              ),
-              onSelected: (String? newValue) {
-                setState(() {
-                  _selectedItem = newValue;
-                });
-              },
-              dropdownMenuEntries:
-                  _dropdownItems.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            )
-          ],
-        )
-      ],
-    );
+    return
   }
 }
