c09081fa925fe589542af8f5e8f2971e0321f904
Your Name <you@example.com>
2024-05-27 17:47:50 +0300

    to be continue

--- textproject/lib/widgets/custom_footer.dart
+++ textproject/lib/widgets/custom_footer.dart
@@ -6,128 +6,101 @@ class CustomFooter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: const Color(0xFF002D62),
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
+      color: const Color(0xFF003969), // Arka plan rengi
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Prüfungscneter',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16.0,
-                      ),
-                    ),
-                    Text(
-                      'eine Marke des Avicenna Institut e. V.',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    Divider(color: Colors.white),
-                    Text(
-                      'Impressum',
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                    Text(
-                      'Datenschutz',
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                    Text(
-                      'Allgemeine Geschäftsbedingungen',
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                    SizedBox(height: 10.0),
-                    Row(
-                      children: [
-                        Icon(Icons.credit_card, color: Colors.white),
-                        SizedBox(width: 5.0),
-                        Icon(Icons.credit_card, color: Colors.white),
-                        SizedBox(width: 5.0),
-                        Icon(Icons.credit_card, color: Colors.white),
-                        SizedBox(width: 5.0),
-                        Icon(Icons.credit_card, color: Colors.white),
-                      ],
-                    ),
-                  ],
+              const Text(
+                "PrüfungCenter",
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFFA6B0BA),
                 ),
               ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Unsere Kooperationspartner',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16.0,
-                      ),
+              Row(children: [
+                Text(
+                  'eine Marke des ',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey[300],
+                  ),
+                ),
+                GestureDetector(
+                  onTap: _onLinkTap,
+                  child: const Text(
+                    'Avicenna Institut e. V.',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.lightBlueAccent,
                     ),
-                    const SizedBox(height: 10.0),
-                    _buildPartnerRow(
-                        'Mainz', 'Akademisches Bildungs-Center e.V.'),
-                    _buildPartnerRow('Homburg', 'Prisma e. V.'),
-                    _buildPartnerRow(
-                        'Kaiserslautern', 'Akademisches Bildungszentrum e.V.'),
-                    _buildPartnerRow(
-                        'Ludwigshafen', 'Lernzirkel Ludwigshafen e.V.'),
-                  ],
+                  ),
+                ),
+              ])
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GestureDetector(
+                onTap: _onLinkTap,
+                child: const Text(
+                  'IMPRESSUM',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.lightBlueAccent,
+                  ),
                 ),
               ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(
-                        height: 28.0), // Padding to align with other columns
-                    _buildPartnerRow(
-                        'Hanau', 'Brüder Grimm Bildungszentrum e.V.'),
-                    _buildPartnerRow('Friedberg', 'LernPoint e. V.'),
-                    _buildPartnerRow(
-                        'Kassel', 'Akademische Bildungsplattform e.V.'),
-                    _buildPartnerRow(
-                        'Wiesbaden', 'Delphin Nachhilfe und Sprachkurse e.V.'),
-                  ],
+              GestureDetector(
+                onTap: _onLinkTap,
+                child: const Text(
+                  'DATENSCHUNTZ',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.lightBlueAccent,
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: _onLinkTap,
+                child: const Text(
+                  'Allgemeine Geschäftsbedingungen',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.lightBlueAccent,
+                  ),
                 ),
               ),
             ],
           ),
-          const Divider(color: Colors.white),
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 8.0),
-            child: Text(
-              '2021 © Alle Rechte vorbehalten',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
+          Column(
+            children: [
+              Image.asset(
+                'assets/paytype/alltype.png',
+                width: 100,
+                height: 100,
+                fit: BoxFit.contain,
+              ),
+              const SizedBox(height: 8),
+              Image.asset(
+                'assets/paytype/sofort.png',
+                width: 50,
+                height: 50,
+                fit: BoxFit.contain,
+              ),
+            ],
+          )
         ],
       ),
     );
   }
 
-  Widget _buildPartnerRow(String city, String partner) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 8.0),
-      child: RichText(
-        text: TextSpan(
-          text: '$city: ',
-          style:
-              const TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-          children: [
-            TextSpan(
-              text: partner,
-              style: const TextStyle(fontWeight: FontWeight.normal),
-            ),
-          ],
-        ),
-      ),
-    );
+  void _onLinkTap() {
+    // Bu işlev, tıklama durumunda yapılacak işlemleri içerir
   }
 }
