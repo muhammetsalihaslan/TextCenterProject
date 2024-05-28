04f258f8ff6b1ca4e0e273457f46c8a66569c43a
Your Name <you@example.com>
2024-05-28 14:27:42 +0300

    footer

--- textproject/lib/widgets/custom_footer.dart
+++ textproject/lib/widgets/custom_footer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CustomFooter extends StatelessWidget {
   const CustomFooter({super.key});
@@ -12,7 +13,7 @@ class CustomFooter extends StatelessWidget {
     return Container(
       color: const Color(0xFF003969), // Arka plan rengi
       padding: EdgeInsets.symmetric(
-          vertical: screenCFHeight * 0.02, horizontal: screenCFWidth * 0.01),
+          vertical: screenCFWidth * 0.01, horizontal: screenCFWidth * 0.01),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
@@ -22,8 +23,10 @@ class CustomFooter extends StatelessWidget {
               Text(
                 "PrüfungCenter",
                 style: TextStyle(
-                  fontSize: screenCFWidth *
-                      0.03, // Ekran genişliğine göre dinamik yazı boyutu
+                  fontSize: screenCFWidth > 1000
+                      ? screenCFWidth * 0.03
+                      : screenCFWidth *
+                          0.05, // Ekran genişliğine göre dinamik yazı boyutu
                   fontWeight: FontWeight.bold,
                   color: const Color(0xFFA6B0BA),
                 ),
@@ -33,8 +36,10 @@ class CustomFooter extends StatelessWidget {
                   Text(
                     'eine Marke des ',
                     style: TextStyle(
-                      fontSize: screenCFWidth *
-                          0.01, // Ekran genişliğine göre dinamik yazı boyutu
+                      fontSize: screenCFWidth > 1000
+                          ? screenCFWidth * 0.01
+                          : screenCFWidth *
+                              0.01, // Ekran genişliğine göre dinamik yazı boyutu
                       color: const Color(0xFFA6B0BA),
                     ),
                   ),
@@ -43,8 +48,10 @@ class CustomFooter extends StatelessWidget {
                     child: Text(
                       'Avicenna Institut e. V.',
                       style: TextStyle(
-                        fontSize: screenCFWidth *
-                            0.01, // Ekran genişliğine göre dinamik yazı boyutu
+                        fontSize: screenCFWidth > 1000
+                            ? screenCFWidth * 0.01
+                            : screenCFWidth *
+                                0.02, // Ekran genişliğine göre dinamik yazı boyutu
                         color: Colors.lightBlueAccent,
                       ),
                     ),
@@ -60,8 +67,10 @@ class CustomFooter extends StatelessWidget {
                     child: Text(
                       'IMPRESSUM',
                       style: TextStyle(
-                        fontSize: screenCFWidth *
-                            0.01, // Ekran genişliğine göre dinamik yazı boyutu
+                        fontSize: screenCFWidth > 1000
+                            ? screenCFWidth * 0.01
+                            : screenCFWidth *
+                                0.02, // Ekran genişliğine göre dinamik yazı boyutu
                         color: Colors.lightBlueAccent,
                       ),
                     ),
@@ -71,8 +80,10 @@ class CustomFooter extends StatelessWidget {
                     child: Text(
                       'DATENSCHUNTZ',
                       style: TextStyle(
-                        fontSize: screenCFWidth *
-                            0.0, // Ekran genişliğine göre dinamik yazı boyutu
+                        fontSize: screenCFWidth > 1000
+                            ? screenCFWidth * 0.01
+                            : screenCFWidth *
+                                0.02, // Ekran genişliğine göre dinamik yazı boyutu
                         color: Colors.lightBlueAccent,
                       ),
                     ),
@@ -82,8 +93,10 @@ class CustomFooter extends StatelessWidget {
                     child: Text(
                       'Allgemeine Geschäftsbedingungen',
                       style: TextStyle(
-                        fontSize: screenCFWidth *
-                            0.01, // Ekran genişliğine göre dinamik yazı boyutu
+                        fontSize: screenCFWidth > 1000
+                            ? screenCFWidth * 0.01
+                            : screenCFWidth *
+                                0.02, // Ekran genişliğine göre dinamik yazı boyutu
                         color: Colors.lightBlueAccent,
                       ),
                     ),
@@ -93,22 +106,28 @@ class CustomFooter extends StatelessWidget {
             ],
           ),
           Padding(
-            padding: EdgeInsets.only(top: screenCFHeight * 0.1),
+            padding: EdgeInsets.only(top: screenCFHeight * 0.05),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Row(
                   children: [
-                    Icon(Icons.phone,
-                        color: Colors.lightBlueAccent,
-                        size: screenCFWidth * 0.01),
+                    Icon(
+                      Icons.phone,
+                      color: Colors.lightBlueAccent,
+                      size: screenCFWidth > 1000
+                          ? screenCFWidth * 0.01
+                          : screenCFWidth * 0.02,
+                    ),
                     const SizedBox(width: 8),
                     Text(
                       '+90 555 555 55 55',
                       style: TextStyle(
-                        fontSize: screenCFWidth *
-                            0.01, // Ekran genişliğine göre dinamik yazı boyutu
+                        fontSize: screenCFWidth > 1000
+                            ? screenCFWidth * 0.01
+                            : screenCFWidth *
+                                0.02, // Ekran genişliğine göre dinamik yazı boyutu
                         color: Colors.white,
                       ),
                     ),
@@ -117,15 +136,21 @@ class CustomFooter extends StatelessWidget {
                 const SizedBox(height: 10), // Bilgiler arasına boşluk ekleyin
                 Row(
                   children: [
-                    Icon(Icons.email,
-                        color: Colors.lightBlueAccent,
-                        size: screenCFWidth * 0.01),
+                    Icon(
+                      Icons.email,
+                      color: Colors.lightBlueAccent,
+                      size: screenCFWidth > 1000
+                          ? screenCFWidth * 0.01
+                          : screenCFWidth * 0.02,
+                    ),
                     const SizedBox(width: 8),
                     Text(
                       'info@example.com',
                       style: TextStyle(
-                        fontSize: screenCFWidth *
-                            0.01, // Ekran genişliğine göre dinamik yazı boyutu
+                        fontSize: screenCFWidth > 1000
+                            ? screenCFWidth * 0.01
+                            : screenCFWidth *
+                                0.02, // Ekran genişliğine göre dinamik yazı boyutu
                         color: Colors.white,
                       ),
                     ),
@@ -134,15 +159,21 @@ class CustomFooter extends StatelessWidget {
                 const SizedBox(height: 10), // Bilgiler arasına boşluk ekleyin
                 Row(
                   children: [
-                    Icon(Icons.location_on,
-                        color: Colors.lightBlueAccent,
-                        size: screenCFWidth * 0.01),
+                    Icon(
+                      Icons.location_on,
+                      color: Colors.lightBlueAccent,
+                      size: screenCFWidth > 1000
+                          ? screenCFWidth * 0.01
+                          : screenCFWidth * 0.02,
+                    ),
                     const SizedBox(width: 8),
                     Text(
                       '1234 Example St, City, Country',
                       style: TextStyle(
-                        fontSize: screenCFWidth *
-                            0.01, // Ekran genişliğine göre dinamik yazı boyutu
+                        fontSize: screenCFWidth > 1000
+                            ? screenCFWidth * 0.01
+                            : screenCFWidth *
+                                0.02, // Ekran genişliğine göre dinamik yazı boyutu
                         color: Colors.white,
                       ),
                     ),
@@ -152,28 +183,52 @@ class CustomFooter extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(top: 90),
-            child: Column(
-              children: [
-                Image.asset(
-                  'assets/paytype/alltype.png',
-                  width: screenCFWidth *
-                      0.01, // Ekran genişliğine göre dinamik boyut
-                  height: screenCFHeight *
-                      0.01, // Ekran genişliğine göre dinamik boyut
-                  fit: BoxFit.contain,
-                ),
-                const SizedBox(height: 8),
-                Image.asset(
-                  'assets/paytype/sofort.png',
-                  width: screenCFWidth *
-                      0.02, // Ekran genişliğine göre dinamik boyut
-                  height: screenCFWidth *
-                      0.03, // Ekran genişliğine göre dinamik boyut
-                  fit: BoxFit.contain,
-                ),
-              ],
-            ),
+            padding: EdgeInsets.only(top: screenCFHeight * 0.05),
+            child: screenCFWidth > 650
+                ? const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.ccVisa, // Visa kart ikonu
+                        size: 50.0,
+                        color: Colors.blue,
+                      ),
+                      SizedBox(width: 20),
+                      Icon(
+                        FontAwesomeIcons.ccMastercard, // Mastercard ikonu
+                        size: 50.0,
+                        color: Colors.red,
+                      ),
+                      SizedBox(width: 20),
+                      Icon(
+                        FontAwesomeIcons.paypal, // PayPal ikonu
+                        size: 50.0,
+                        color: Colors.blueAccent,
+                      ),
+                    ],
+                  )
+                : const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.ccVisa, // Visa kart ikonu
+                        size: 30.0,
+                        color: Colors.blue,
+                      ),
+                      SizedBox(height: 2),
+                      Icon(
+                        FontAwesomeIcons.ccMastercard, // Mastercard ikonu
+                        size: 30.0,
+                        color: Colors.red,
+                      ),
+                      SizedBox(height: 2),
+                      Icon(
+                        FontAwesomeIcons.paypal, // PayPal ikonu
+                        size: 30.0,
+                        color: Colors.blueAccent,
+                      ),
+                    ],
+                  ),
           )
         ],
       ),
